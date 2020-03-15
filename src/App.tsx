ec4281d0766dd67fcd479f6523f83016eeee6774
Gabriel Mouallem <gabriel.unifei2017@gmail.com>
2020-03-15 19:26:46 -0300

    small adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Nav from './pages/DashBoard/components/Nav/Nav';
+import Map from './pages/DashBoard/components/Map/Map';
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Map />
     </div>
   );
 }
--- src/pages/DashBoard/components/Nav/Nav.tsx
+++ src/pages/DashBoard/components/Nav/Nav.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './Nav.scss';
 
-
 export default () => {
 
     return (
