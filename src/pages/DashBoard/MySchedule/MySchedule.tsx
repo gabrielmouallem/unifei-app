2b7bc45db536ca718178b35c9bb386e26cd7e468
Gabriel José Mouallem Rodrigues <40147856+gabrielmouallem@users.noreply.github.com>
2020-06-24 00:30:27 -0300

    Made the pdf read works without encoding props

--- src/pages/DashBoard/MySchedule/MySchedule.tsx
+++ src/pages/DashBoard/MySchedule/MySchedule.tsx
@@ -4,34 +4,37 @@ import { Button } from '@material-ui/core';
 import AbsoluteWrapper from '../../../components/AbsoluteWrapper/AbsoluteWrapper';
 import { Plugins, FilesystemDirectory, FilesystemEncoding } from '@capacitor/core';
 const { Filesystem } = Plugins;
+const { Clipboard } = Plugins;
 
 export default () => {
 
   const [pdf, setPdf] = useState(undefined);
 
-  async function fileRead() {
-    let contents = await Filesystem.readFile({
-      path: String('Sistema Integrado de Gestão de Atividades Acadêmicas.pdf'),
-      directory: FilesystemDirectory.Documents,
-      encoding: FilesystemEncoding.UTF8
-    });
-    // @ts-ignore
-    setPdf(contents.data);
-    alert(JSON.stringify(contents));
-  }
-
   async function fileWrite() {
     try {
       const result = await Filesystem.writeFile({
-        path: 'TESTANDO.txt',
+        path: 'TESTE.PDF',
         data: "This is a test",
         directory: FilesystemDirectory.Documents,
         encoding: FilesystemEncoding.UTF8
       })
-      alert('Wrote file '+ JSON.stringify(result));
+      console.log('Wrote file', result);
     } catch(e) {
-      alert('Unable to write file ' + JSON.stringify(e));
+      console.error('Unable to write file', e);
     }
+  }
+
+  async function fileRead() {
+    let contents = await Filesystem.readFile({
+      path: String('Sistema Integrado de Gestão de Atividades Acadêmicas.pdf'),
+      directory: FilesystemDirectory.Documents,
+    });
+    const result = await Filesystem.writeFile({
+      path: 'TESTE4.PDF',
+      data: contents.data,
+      directory: FilesystemDirectory.Documents,
+    })
+    console.log('Wrote file', result);
   }
 
   return (
@@ -39,14 +42,15 @@ export default () => {
       <div className="my-schedule">
         <Button variant="contained"
           onClick={() => {
-            alert(String('Sistema Integrado de Gestão de Atividades Acadêmicas.pdf').replace(/ /g,"%20"))
-            fileRead().then(res=>{
+            alert(String('Sistema Integrado de Gestão de Atividades Acadêmicas.pdf').replace(/ /g, "%20"))
+            fileRead().then(res => {
               alert(res)
-            }).catch(err=>{
+            }).catch(err => {
               alert(err)
             })
           }}>
-                </Button>
+            ----------------- IMPORTAR -----------------
+        </Button> 
       </div>
     </AbsoluteWrapper>
   )
