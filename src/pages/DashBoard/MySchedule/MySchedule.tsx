f213d3da155f1f3fa74dd7eea6024d4ab1164332
Gabriel José Mouallem Rodrigues <40147856+gabrielmouallem@users.noreply.github.com>
2020-06-27 20:28:10 -0300

    improving my schedule page styles

--- src/pages/DashBoard/MySchedule/MySchedule.tsx
+++ src/pages/DashBoard/MySchedule/MySchedule.tsx
@@ -1,15 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './MySchedule.scss';
 import { Button } from '@material-ui/core';
 import AbsoluteWrapper from '../../../components/AbsoluteWrapper/AbsoluteWrapper';
 import { FileChooser } from '@ionic-native/file-chooser';
 import { Plugins, FilesystemDirectory, FilesystemEncoding } from '@capacitor/core';
+import { useHistory } from 'react-router-dom';
+import BaseModal from '../../../components/BaseModal/BaseModal';
 const { Filesystem } = Plugins;
 const { Clipboard } = Plugins;
 
 export default () => {
 
+  const currentMonth = new Date().getMonth();
+
+  const semester = currentMonth >= 6 ? 2 : 1;
+
   const [pdf, setPdf] = useState(undefined);
+
+  const [open, setOpen] = useState(true);
+
+  const history = useHistory();
 
   async function fileWrite() {
     try {
@@ -38,27 +48,39 @@ export default () => {
     console.log('Wrote file', result);
   }
 
+  useEffect(() => {
+    if (!open) {
+      history.goBack();
+    }
+  }, [open])
+
   return (
     <AbsoluteWrapper>
-      <div className="my-schedule">
-        <Button variant="contained"
-          onClick={() => {
-            FileChooser.open()
-              .then(uri => {
-                alert(uri); 
-                var result = Filesystem.readFile({
-                  path: uri,
-                }).then(res=>{
-                  alert('Arquivo localizado ' + res);
-                }).catch(err=> {
-                  alert("Erro " + err);
-                });
-              })
-              .catch(e => alert(e));
-          }}>
-          ----------------- IMPORTAR -----------------
-        </Button>
-      </div>
+      <BaseModal setOpen={setOpen} title={`Horários ${new Date().getFullYear()}.${semester}`} closeIconDirection="down">
+        <div className="my-schedule">
+          <div className="my-schedule__empty">
+            Você ainda não importou seus horários, gere seu atestado de matrícula no Sigaa em formato PDF
+            e importe seus horário em nosso app clicando no botão abaixo.
+          </div>
+          <Button variant="contained" className="my-schedule__btn-add"
+            onClick={() => {
+              FileChooser.open()
+                .then(uri => {
+                  alert(uri);
+                  var result = Filesystem.readFile({
+                    path: uri,
+                  }).then(res => {
+                    alert('Arquivo localizado ' + res);
+                  }).catch(err => {
+                    alert("Erro " + err);
+                  });
+                })
+                .catch(e => alert(e));
+            }}>
+            IMPORTAR
+            </Button>
+        </div>
+      </BaseModal>
     </AbsoluteWrapper>
   )
 }
