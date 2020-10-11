02367c51e1b62cc1bbe56c341ded7058394e9194
Gabriel Mouallem <gabriel.unifei2017@gmail.com>
2020-10-11 02:58:13 -0300

    Schedule Feature partially re-implemented.

--- src/pages/DashBoard/MySchedule/MySchedule.tsx
+++ src/pages/DashBoard/MySchedule/MySchedule.tsx
@@ -1,31 +1,94 @@
 import React, { useState, useEffect } from 'react';
 import './MySchedule.scss';
-import { Button } from '@material-ui/core';
+import { Button, CircularProgress, ExpansionPanel, ExpansionPanelDetails, ExpansionPanelSummary } from '@material-ui/core';
 import AbsoluteWrapper from '../../../components/AbsoluteWrapper/AbsoluteWrapper';
 import { FileChooser } from '@ionic-native/file-chooser';
 import { Plugins, FilesystemDirectory, FilesystemEncoding } from '@capacitor/core';
 import { useHistory } from 'react-router-dom';
 import BaseModal from '../../../components/BaseModal/BaseModal';
+import { coreHTTPClient } from '../../../services/webclient';
+import useNotify from '../../../hooks/tools/useNotify';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 const { Filesystem } = Plugins;
-const { Clipboard } = Plugins;
 
 export default () => {
+
+  const notify = useNotify();
 
   const currentMonth = new Date().getMonth();
 
   const semester = currentMonth >= 6 ? 2 : 1;
 
-  const [pdf, setPdf] = useState(undefined);
-
   const [open, setOpen] = useState(true);
 
   const history = useHistory();
+
+  const [scheduleArray, setScheduleArray] = useState<any>(undefined);
+
+  const [loading, setLoading] = useState(false);
+
+  const postPdf = (pdf: any) => {
+    coreHTTPClient.post(`pdf/`, pdf).then((res: any) => {
+      setScheduleArray(res.data.data);
+      localStorage.setItem("my-schedule", JSON.stringify(res.data));
+    }).catch(err => {
+      console.log("Erro em postPdf", err);
+    }).finally(()=>{
+      setLoading(false);
+    })
+  }
+
+  useEffect(() => {
+    var tempSchedule = localStorage.getItem("my-schedule");
+    if (tempSchedule) {
+      setScheduleArray(JSON.parse(tempSchedule));
+    }
+  }, [])
 
   useEffect(() => {
     if (!open) {
       history.goBack();
     }
   }, [open])
+
+  if (loading) {
+    return (
+      <AbsoluteWrapper>
+        <BaseModal setOpen={setOpen} title={`Horários ${new Date().getFullYear()}.${semester}`} closeIconDirection="down">
+          <div className="my-schedule">
+            <CircularProgress style={{ color: "rgb(250, 97, 12)", opacity: "1 !important" }} />
+          </div>
+        </BaseModal>
+      </AbsoluteWrapper>
+    )
+  }
+
+  if (scheduleArray) {
+    return (
+      <AbsoluteWrapper>
+        <BaseModal setOpen={setOpen} title={`Horários ${new Date().getFullYear()}.${semester}`} closeIconDirection="down">
+          <div className="my-schedule">
+            {scheduleArray.map((schedule: any, index: number) => {
+              return (
+              <ExpansionPanel style={{marginBottom: "15px", width: "70vw"}}>
+                <ExpansionPanelSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls={`panel1a-content-${index}`}
+                  id={`panel1a-header-${index}`}
+                >
+                  <b>{schedule.codigo}</b>
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails>
+                  {schedule.codigo_horario}{' - '}{schedule.local}
+                </ExpansionPanelDetails>
+              </ExpansionPanel>
+              )
+            })}
+          </div>
+        </BaseModal>
+      </AbsoluteWrapper>
+    )
+  }
 
   return (
     <AbsoluteWrapper>
@@ -37,18 +100,20 @@ export default () => {
           </div>
           <Button variant="contained" className="my-schedule__btn-add"
             onClick={() => {
+              setLoading(true);
               FileChooser.open()
                 .then(uri => {
-                  alert(uri);
-                  var result = Filesystem.readFile({
+                  Filesystem.readFile({
                     path: uri,
                   }).then(res => {
-                    
+                    postPdf(res.data);
                   }).catch(err => {
-                    
+                    notify("Erro ao importar PDF.", "error");
                   });
                 })
-                .catch(e => alert(e));
+                .catch(e => {
+
+                })
             }}>
             IMPORTAR
             </Button>
